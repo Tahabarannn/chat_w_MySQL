3c1b9c7852865e20daad341a2e818b121e75bdc5
Tahabarannn <tahabaranakti10@gmail.com>
2024-04-03 00:41:14 +0300

    Build GUI

--- src/app.py
+++ src/app.py
@@ -5,3 +5,16 @@ load_dotenv()
 
 st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:")
 
+st.title("Chat with MySQL")
+
+with st.sidebar:
+    st.subheader("Settings")
+    st.write("This is a chat application that uses MySQL as a backend. Connect to the database and start chatting.")
+
+    st.text_input("Host", value="localhost")
+    st.text_input("Port", value="3306")
+    st.text_input("User", value="root")
+    st.text_input("Password", type="password", value="admin")
+    st.text_input("Database", value="chat")
+
+    st.button("Connect")
