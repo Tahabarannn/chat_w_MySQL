ea99f723cce61e755ff6f0474807a309dfbb49f7
Tahabarannn <tahabaranakti10@gmail.com>
2024-04-04 00:58:09 +0300

    Make Chat Interactive.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,21 @@
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.utilities import SQLDatabase
 import streamlit as st
+def init_database(
+        user: str,
+        password: str,
+        host: str,
+        port: str,
+        database: str
+):
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage("Hello! I'm a chatbot. How can I help you?"),
+    ]
 
 load_dotenv()
 
@@ -11,10 +27,42 @@ with st.sidebar:
     st.subheader("Settings")
     st.write("This is a chat application that uses MySQL as a backend. Connect to the database and start chatting.")
 
-    st.text_input("Host", value="localhost")
-    st.text_input("Port", value="3306")
-    st.text_input("User", value="root")
-    st.text_input("Password", type="password", value="admin")
-    st.text_input("Database", value="chat")
+    st.text_input("Host", value="localhost", key="Host")
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="root", key="User")
+    st.text_input("Password", type="password", value="admin", key="Password")
+    st.text_input("Database", value="chat", key="Database")
 
-    st.button("Connect")
+    if st.button("Connect"):
+        with st.spinner("Connecting to the database..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"]
+            )
+            st.session_state.db = db
+            st.success("Connected to the database.")
+
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+
+user_query = st.chat_input("Type a message...")
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        response = "I'm sorry, I don't understand that."
+        st.markdown(response)
+
+        st.session_state.chat_history.append(AIMessage(content=response))
