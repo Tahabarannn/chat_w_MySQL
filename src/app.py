57f271bbf706ea5a3197e16e81fa5ad02b78cf8b
Tahabarannn <tahabaranakti10@gmail.com>
2024-04-04 14:50:37 +0300

    SQL Chain created.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.utilities import SQLDatabase
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
 import streamlit as st
 def init_database(
         user: str,
@@ -11,6 +15,46 @@ def init_database(
 ):
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
+
+def get_sql_chain(db):
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's 
+    database. Based on the table schema below, write a SQL query that would answer the user's question. Take the 
+    conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    llm = ChatOpenAI(model="gpt-4-0125-preview")
+
+    def get_schema():
+        return db.get_table_info()
+
+    return(
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm
+        | StrOutputParser()
+
+    )
+
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
@@ -62,7 +106,11 @@ if user_query is not None and user_query.strip() != "":
         st.markdown(user_query)
 
     with st.chat_message("AI"):
-        response = "I'm sorry, I don't understand that."
+        sql_chain = get_sql_chain(st.session_state.db)
+        response =  sql_chain.invoke({
+            "chat_history": st.session_state.chat_history
+            "question": user_query,
+        })
         st.markdown(response)
 
         st.session_state.chat_history.append(AIMessage(content=response))
